09060b85fe3d0735f2e69742264fff00f13bfccd
pedronunomacedo <pedronunomacedo@hotmail.com>
2023-08-06 23:13:36 +0100

    Tried to remove the side scrollbars from all navigators

--- src/components/ProjectsCarousel.jsx
+++ src/components/ProjectsCarousel.jsx
@@ -1,7 +1,5 @@
 import React, { useState } from 'react'
 import Slider from 'react-slick'
-import { Carousel } from "@material-tailwind/react"
-import { FaArrowLeft, FaArrowRight } from 'react-icons/fa'
 import "slick-carousel/slick/slick.css"
 import "slick-carousel/slick/slick-theme.css"
 import "./ProjectsCarousel.css"
@@ -32,10 +30,10 @@ const ProjectsCarousel = () => {
                         alt='image 1'
                         className='h-auto w-full object-cover rounded-lg'
                     />
-                    <div className='project-hover-content absolute top-0 left-0 w-full h-full bg-black bg-opacity-70 text-white flex items-center justify-center opacity-0 transition-opacity px-12 overflow-scroll'>
-                        <div className='text-center overflow-scroll'> {/* flex items-center */}
+                    <div className='project-hover-content absolute top-0 left-0 w-full h-full bg-black bg-opacity-70 text-white flex items-center justify-center opacity-0 transition-opacity px-12 overflow-scroll no-scrollbar'>
+                        <div className='text-center overflow-scroll no-scrollbar'> {/* flex items-center */}
                             <h3 className='text-xl font-bold mb-2 project-title'>Tech4You Project</h3>
-                            <p className='text-sm project-explanation overflow-scroll'>
+                            <p className='text-sm project-explanation overflow-scroll no-scrollbar'>
                                 Project done during a university class, developed by a group of students and me.
                                 This projects was developed using the Laravel framework and it consists of an online shop.
                             </p>
@@ -48,10 +46,10 @@ const ProjectsCarousel = () => {
                         alt='image 2'
                         className='h-auto w-full justify-center rounded-lg'
                     />
-                    <div className='project-hover-content absolute top-0 left-0 w-full h-full bg-black bg-opacity-70 text-white flex items-center justify-center opacity-0 transition-opacity px-12 overflow-scroll'>
-                        <div className='text-center overflow-scroll'> {/* flex items-center */}
+                    <div className='project-hover-content absolute top-0 left-0 w-full h-full bg-black bg-opacity-70 text-white flex items-center justify-center opacity-0 transition-opacity px-12 overflow-scroll no-scrollbar'>
+                        <div className='text-center overflow-scroll no-scrollbar'> {/* flex items-center */}
                             <h3 className='text-xl font-bold mb-2 project-title'>Compiler Project</h3>
-                            <p className='text-sm project-explanation overflow-scroll'>
+                            <p className='text-sm project-explanation overflow-scroll no-scrollbar'>
                                 This project was developed using the Java language and it consists of a compiler that 
                                 translates a custom language into OLLIR code, then to Jasmin code and, finally, to Bytecode, 
                                 in order to be compiled.
